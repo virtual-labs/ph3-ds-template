d15be8931e1332e2e815b1a8921adbed466ef0b5
balammaboya <balamma38@gmail.com>
2021-02-19 16:47:41 +0530

    added directory structure

--- experiment/pre-test/pretest.js
+++ experiment/pre-test/pretest.js
@@ -89,24 +89,47 @@
 
 
      const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
+            question: "1. Sample question1", ///// Write the question inside double quotes
             answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+                a: "1", ///// Write the option 1 inside double quotes
+                b: "2", ///// Write the option 2 inside double quotes
+                c: "3", ///// Write the option 2 inside double quotes
+                d: "4", ///// Write the option 2 inside double quotes
             },
-            correctAnswer: "a" ///// Write the correct option inside double quotes
+            correctAnswer: "b" ///// Write the correct option inside double quotes
         },
 
     {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
+      question: "2. Sample question2",  ///// Write the question inside double quotes
       answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
-      correctAnswer: "c"                ///// Write the correct option inside double quotes
+        a: "1",                  ///// Write the option 1 inside double quotes
+        b: "2",                  ///// Write the option 2 inside double quotes
+	c: "3", ///// Write the option 3 inside double quotes
+        d: "4", ///// Write the option 4 inside double quotes
+             },
+      correctAnswer: "a"                ///// Write the correct option inside double quotes
     },
 
-
+{
+      question: "3. Sample question3",  ///// Write the question inside double quotes
+      answers: {
+        a: "1",                  ///// Write the option 1 inside double quotes
+        b: "2",                  ///// Write the option 2 inside double quotes
+	c: "3", ///// Write the option 3 inside double quotes
+        d: "4", ///// Write the option 4 inside double quotes
+             },
+      correctAnswer: "d"                ///// Write the correct option inside double quotes
+    },
+{
+      question: "4. Sample question4",  ///// Write the question inside double quotes
+      answers: {
+        a: "1",                  ///// Write the option 1 inside double quotes
+        b: "2",                  ///// Write the option 2 inside double quotes
+	c: "3", ///// Write the option 3 inside double quotes
+        d: "4", ///// Write the option 4 inside double quotes
+             },
+      correctAnswer: "c"                ///// Write the correct option inside double quotes
+    },
 
      ];
 
